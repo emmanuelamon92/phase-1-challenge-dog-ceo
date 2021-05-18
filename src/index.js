b8aeb59467726e24d8b70c6d561abb61a711aa94
Emmanuel <emmanuelamon92@gmail.com>
2021-05-18 00:21:18 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+
+// const imgContainer = document.querySelector('#dog-image-container')
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+        .then(response => response.json())
+        .then(json => {
+            const imgContainer = document.querySelector('#dog-image-container')
+            console.log(json.message);
+            json.message.forEach(image => {
+                const img = document.createElement('img');
+                // img.scr = image;
+                imgContainer.appendChild(img);
+            })
+        })
+})
